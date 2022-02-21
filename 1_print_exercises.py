c3f526b02d69f045b20006dbb1eda1c482f499cc
BuildTools <unconfigured@null.spigotmc.org>
2022-02-22 12:13:45 +1300

    did first exercise

---
+++ 1_print_exercises.py
@@ -0,0 +1,8 @@
+#1 print out name
+print("Luke Smith")
+
+#2 teacher name
+print("Mr Smith")
+
+#3 name of school
+print("King's High School")
---
+++ helloworld.py
@@ -0,0 +1,5 @@
+#this code prints out hello world
+print("hello world")
+print(5+5)
+print(10*60)
+print("12318971/123178")
